3210a1fb1d9dd67767e82f5cab45b06c487806ec
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-17 13:31:41 -0600

    initial

---
+++ src/routes/achievement-17/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let buttonID = 1;
+
+  const handleClick = function () {
+    buttonID++;
+    console.log(buttonID);
+  };
+</script>
+
+<main>
+  <h1>Achievement 17 - Push of a Button</h1>
+  <div on:click={handleClick}>
+    <button>CLICK ME! {buttonID}</button>
+  </div>
+</main>
+
+<style>
+  div {
+    text-align: center;
+    padding: 2rem;
+  }
+
+  h1 {
+    text-align: center;
+    padding: 7rem;
+  }
+</style>
